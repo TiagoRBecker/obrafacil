dead3ab93c9639ae9378b5724024d2773e8d1359
Tiago Becker <84158213+TiagoRBecker@users.noreply.github.com>
2026-05-28 21:47:37 +0000

    remove pasta user

--- src/modules/Users/repo/user.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { UserEntity } from '../entity/user.entity';
-
-export abstract class UserRepositoryInterface {
-  abstract findByEmail(email: string): Promise<UserEntity | null>;
-   abstract findById(id: string): Promise<UserEntity | null>;
-  abstract create(user: UserEntity): Promise<UserEntity>;
-  
-}

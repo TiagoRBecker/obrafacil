fd61d292613363826685a82fbc29d5377dfd51e8
Tiago Becker <beckertiago09@gail.com>
2026-06-11 16:02:42 -0300

    correcoes

--- src/modules/auth/controllers/signin.controller.ts
+++ src/modules/auth/controllers/signin.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
+
+import { SignInUseCase } from '../usecase/sign-in.usecase';
 import { SignInDto } from '../../users/dto/sign-in.dto';
 import { AuthResponseDto } from '../../users/dto/auth-response.dto';
-import { SignInUseCase } from '../usecase/sign-in.usecase';
 
 @ApiTags('Autenticação')
 @Controller('auth')

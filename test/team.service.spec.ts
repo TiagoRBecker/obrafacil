a77d055c9c15a6b6936a0e6427a2333cb5ec98da
Tiago Becker <beckertiago09@gail.com>
2026-06-16 10:05:05 -0300

    unit test create team

--- test/team.service.spec.ts
+++ test/team.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConflictException, NotFoundException } from '@nestjs/common';
 import { InMemoryTeamMemberRepository } from '../src/modules/team/repo/in-memory-team-member.repository';
 import { CreateTeamMemberUseCase } from '../src/modules/team/usecase/create-team-member.usecase';
 import { teamMock } from './helpers/mocks/team';
@@ -15,19 +16,42 @@ describe('Budgets  (Testes Unitários)', () => {
     // 2. Injetamos o repositório direto no serviço (Injeção de Dependência manual)
     create = new CreateTeamMemberUseCase(repository);
   });
-  describe('Teste  unitarios time', () => {
+  describe('Teste  unitarios team', () => {
     it('Deve criar um colaborador ', async () => {
       const newTeam = await create.execute(teamMock);
-      
+
       expect(newTeam.id).toBeDefined();
-        expect(newTeam).toHaveProperty('id');
+      expect(newTeam).toHaveProperty('id');
       expect(newTeam).toMatchObject({
         name: teamMock.name,
         email: teamMock.email,
         phone: teamMock.phone,
         jobTitle: teamMock.jobTitle,
-    
       });
+    });
+    it('Deve falhar ao criar um colaborador email já cadastrado  ', async () => {
+      const newTeam = await create.execute(teamMock);
+
+      const newMockTeam = {
+        ...teamMock,
+        email: newTeam.email,
+      };
+
+      await expect(create.execute(newMockTeam)).rejects.toBeInstanceOf(
+        ConflictException,
+      );
+    });
+    it('Deve falhar ao criar um colaborador telefone já cadastrado  ', async () => {
+      const newTeam = await create.execute(teamMock);
+
+      const newMockTeam = {
+        ...teamMock,
+        phone: newTeam.phone as string,
+      };
+
+      await expect(create.execute(newMockTeam)).rejects.toBeInstanceOf(
+        ConflictException,
+      );
     });
   });
 });
